d163e50ea063c18e2f8fed534662cf2a3aca270b
RositcaMadzharova <val.ros@abv.bg>
2018-02-03 19:44:42 +0200

    -

---
+++ Exchange Variable Values.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exchange_Variable_Values
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            Console.WriteLine("Before:");
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+
+            int oldA = a;
+            a = b;
+            b = oldA;
+            Console.WriteLine("After:");
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+        }
+    }
+}
