de6510d6a41f355f086984d5c87c23ca2bd70ae1
RositcaMadzharova <val.ros@abv.bg>
2018-02-03 19:25:09 +0200

    -

---
+++ Boolean Variable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Boolean_Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            bool boolean = Convert.ToBoolean(input);
+
+            if (boolean)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
