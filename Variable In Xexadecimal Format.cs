1aef886d6bdf74422663c38faf1d0ef2fcbc4b02
RositcaMadzharova <val.ros@abv.bg>
2018-02-03 19:16:02 +0200

    -

---
+++ Variable In Xexadecimal Format.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+
+namespace ConsoleNETCore
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            string hexadecimal = Console.ReadLine();
+
+            int integer = Convert.ToInt32(hexadecimal, 16);
+
+            Console.WriteLine(integer);
+
+        }
+
+    }
+
+}
+
+
