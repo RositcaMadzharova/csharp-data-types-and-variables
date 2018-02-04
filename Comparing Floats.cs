816d68c73bf3c4485490ef21dbc58bbc8f02880c
RositcaMadzharova <val.ros@abv.bg>
2018-02-04 11:11:32 +0200

    -

---
+++ Comparing Floats.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Comparing_Floats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double first = double.Parse(Console.ReadLine());
+            double second = double.Parse(Console.ReadLine());
+            double differense = Math.Abs(first - second);
+            double eps = 0.000001;
+
+            if (differense < eps)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
