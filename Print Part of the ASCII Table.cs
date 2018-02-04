dee847fa9254f33b2cbc51173c7f7bc22f86bab1
RositcaMadzharova <val.ros@abv.bg>
2018-02-04 11:20:47 +0200

    -

---
+++ Print Part of the ASCII Table.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Print_Part_of_the_ASCII_Table
+{
+    class PrintPartOfTheASCIITable
+    {
+        static void Main(string[] args)
+        {
+            byte start = byte.Parse(Console.ReadLine());
+            byte end = byte.Parse(Console.ReadLine());
+
+            for (byte i = start; i <= end; i++)
+            {
+                Console.WriteLine($"{(char) i}");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
