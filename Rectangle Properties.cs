544f02b48914fc905b130b8750fe8bab4591a3f7
RositcaMadzharova <val.ros@abv.bg>
2018-02-04 10:14:40 +0200

    -

---
+++ Rectangle Properties.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Rectangle_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double height = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+
+            double perimeter = height * 2 + width * 2;
+            double area = height * width;
+            double diagonal = Math.Sqrt(height * height + width * width);
+
+            Console.WriteLine(perimeter);
+            Console.WriteLine(area);
+            Console.WriteLine(diagonal);
+        }
+    }
+}
