477cbf2c29c992e8427901363dbf163a0ab28897
RositcaMadzharova <val.ros@abv.bg>
2018-02-04 12:39:14 +0200

    -

---
+++ Thea the Photographer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Thea_the_Photographer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalPicturesTaken = int.Parse(Console.ReadLine());
+            int filterTimeSeconds = int.Parse(Console.ReadLine());
+            int filterFactor = int.Parse(Console.ReadLine());
+            int uploadTimeSeconds = int.Parse(Console.ReadLine());
+
+            int filteredPictures = (int)Math.Ceiling(((double)totalPicturesTaken * filterFactor) / 100);
+            long totalFilterTimeSeconds = (long)totalPicturesTaken * filterTimeSeconds;
+            long totalUploadTimeSeconds = (long)filteredPictures * uploadTimeSeconds;
+
+            TimeSpan totalTime = TimeSpan.FromSeconds(totalUploadTimeSeconds + totalFilterTimeSeconds);
+            string totalTimeFormatted = totalTime.ToString(@"d\:hh\:mm\:ss");
+            Console.WriteLine(totalTimeFormatted);
+        }
+    }
+}
